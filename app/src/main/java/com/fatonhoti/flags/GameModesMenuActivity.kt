0ff05b47735a43bd8de83511d4a8dd903f652ba2
Faton Hoti <hotifaton@outlook.com>
2022-04-15 16:08:42 +0200

    Experiment with implementation of color scheme for light and dark mode, wip

--- app/src/main/java/com/fatonhoti/flags/GameModesMenuActivity.kt
+++ app/src/main/java/com/fatonhoti/flags/GameModesMenuActivity.kt
@@ -1,10 +1,7 @@
 package com.fatonhoti.flags
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -13,6 +10,9 @@ class GameModesMenuActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game_modes_menu)
 
+        // TODO: Make buttons in each card in the RecyclerView actually clickable and reactive
+        // TODO: Make cards in RecyclerView actually display suitable images
+
         val items = mutableListOf<GameModeCard>()
         items.add(GameModeCard("FLAGS", "Do you know all the flags?"))
         items.add(GameModeCard("CAPITALS", "What about the capitals?"))
@@ -20,7 +20,6 @@ class GameModesMenuActivity : AppCompatActivity() {
 
         val recyclerView: RecyclerView = findViewById(R.id.rvCards)
         val adapter = GameModeCardAdapter(items)
-
         recyclerView.layoutManager = LinearLayoutManager(this)
         recyclerView.adapter = adapter
 
